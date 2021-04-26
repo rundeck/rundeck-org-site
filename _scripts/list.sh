5bd32e3dd2c3c991698f5f5cbd615792b19a7f00
ProTip <greg.zapp@gmail.com>
2021-04-26 21:22:53 +0000

    Update package download instructions and list for new locations

--- _scripts/list.sh
+++ _scripts/list.sh
@@ -2,8 +2,7 @@
 
 IFS=$'\n\t'
 
-for i in deb rpm war jar; do
-    S3_LIST=$(aws s3 ls s3://download.rundeck.org/$i/ | sort -k 4,4 -V -r)
+make_list() {
     LIST=$(
     while read -r item; do
         DATE=$(echo $item | awk '{print $1}')
@@ -13,7 +12,21 @@ for i in deb rpm war jar; do
         FILE=$(echo $item | awk '{print $4}')
 
         echo "* [$FILE](https://download.rundeck.org/$i/$FILE) ($SIZE bytes) - $DATE $TIME"
-    done <<< "$S3_LIST")
-    echo "${LIST}" > ./download/$i/_list.md
-    cat ./download/$i/_index.md ./download/$i/_list.md > ./download/$i/index.md
+    done <<< "$2")
+    echo "${LIST}"
+}
+
+write_list() {
+    echo "${2}" > ./download/$1/_list.md
+    cat ./download/$1/_index.md ./download/$1/_list.md > ./download/$1/index.md
+}
+
+for i in deb war jar; do
+    S3_LIST="$(make_list $i "$(aws s3 ls s3://download.rundeck.org/$i/ | sort -k 4,4 -V -r)")"
+    write_list "${i}" "${S3_LIST}"
 done
+
+# RPM
+S3_LIST=$(make_list rpm "$(aws s3 ls s3://download.rundeck.org/rpm/ | sort -k 4,4 -V -r | grep -v config)")
+S3_LIST+=$(make_list rpm "$(aws s3 ls s3://download.rundeck.org/rpm/ | sort -k 4,4 -V -r | grep config)")
+write_list rpm "${S3_LIST}"
